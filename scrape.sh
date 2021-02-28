153c658861d6cf6011c86973ebb448725ff9471f
Alex Hayward <alexhayward@alexs-air.mynet>
2021-02-28 14:16:17 +0000

    csv

--- scrape.sh
+++ scrape.sh
@@ -5,7 +5,8 @@ CURL_RESULT=/tmp/curl_result.json
 LAST_RESULT=/tmp/last_result.json
 MERGE_RESULT=/tmp/merge_result.json
 
-OUTPUT_FILE=./greatest_500_albums.json
+OUTPUT_FILE_JSON=./greatest_500_albums.json
+OUTPUT_FILE_CSV=./greatest_500_albums.csv
 
 get_links () {
 	curl https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063 \
@@ -23,6 +24,8 @@ execute () {
 			| grep pmcGalleryExports \
 			| sed "s/var pmcGalleryExports = \(.*\);/\1/" \
 			| jq '[.gallery[] | {"rank": .positionDisplay, "title": .title, "description": .description, "coverUrl": .image}]' \
+			| sed -e 's/<[^>]*>//g' \
+			| sed 's/\\n//g' \
 			> $CURL_RESULT
 
 		jq -s '[ map(.[]) ]|flatten' $LAST_RESULT $CURL_RESULT > $MERGE_RESULT
@@ -30,7 +33,15 @@ execute () {
 		cat $MERGE_RESULT > $LAST_RESULT
 	done < $LINKS_FILE
 
-	mv $LAST_RESULT $OUTPUT_FILE
+	mv $LAST_RESULT $OUTPUT_FILE_JSON
+}
+
+create_csv() {
+	# csv headers
+	echo "rank, title, description, coverUrl" > $OUTPUT_FILE_CSV
+
+	# csv data
+	jq -r '.[]|[.rank, .title, .description, .coverUrl]|@csv' $OUTPUT_FILE_JSON >> $OUTPUT_FILE_CSV
 }
 
 cleanup () {
@@ -42,8 +53,9 @@ cleanup () {
 main () {
 	get_links 2> /dev/null
 	execute 2> /dev/null
-	RESULT=$(cat $OUTPUT_FILE | jq '.[] | .rank' | sort | uniq -c | wc -l)
-	echo "$RESULT albums processed and added to ./greatest_500_albums.json"
+	create_csv 2> /dev/null
+	RESULT=$(cat $OUTPUT_FILE_JSON | jq '.[] | .rank' | sort | uniq -c | wc -l)
+	echo "$RESULT albums processed and added to ./greatest_500_albums.json and ./greatest_500_albums.csv"
 }
 
 main
