8b2381ddd759dd48de5942d6a1ffdaa7bc0be981
Alex Hayward <alexhayward@alexs-air.mynet>
2021-02-28 13:52:19 +0000

    removed hardcoded links

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 500 Greatest Albums of All Time
 
+https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063
+
 I'm going to try and listen to all of them, but Rolling Stone's website makes my computer unhappy, and I want to be able to add ratings and things in Airtable.
 
-Script just goes through each page, pulls out a few fields, and stitches everything together in a single JSON file. Whoever at Rolling Stone decided to just keep all the album info in a JSON object on each page is a hero.
+Script just pulls in the links to each range of albums, goes through each page, pulls out a few fields, and stitches everything together in a single JSON file. Whoever at Rolling Stone decided to just keep all the album info in a JSON object on each page is a hero.
--- scrape.sh
+++ scrape.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
 
-declare -a urls=("https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/rufus-chaka-khan-ask-rufus-1062734/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/linda-mccartney-and-paul-ram-1062783/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/the-go-gos-beauty-and-the-beat-1062833/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/stevie-wonder-music-of-my-mind-2-1062883/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/shania-twain-come-on-over-1062933/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/buzzcocks-singles-going-steady-2-1062983/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/sade-diamond-life-1063033/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/bruce-springsteen-nebraska-3-1063083/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/the-band-music-from-big-pink-2-1063133/"
-				"https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/jay-z-the-blueprint-3-1063183/"
-				)
+LINKS_FILE=/tmp/pages.txt
+CURL_RESULT=/tmp/curl_result.json
+LAST_RESULT=/tmp/last_result.json
+MERGE_RESULT=/tmp/merge_result.json
+
+OUTPUT_FILE=./greatest_500_albums.json
+
+get_links () {
+	curl https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063 \
+			| grep pmcGalleryExports \
+			| sed "s/var pmcGalleryExports = \(.*\);/\1/" \
+			| jq '.listNavBar.generatedRanges."800"[] | .link' \
+			| sed "s/\"//g" \
+			> $LINKS_FILE
+}
 
 execute () {
-	touch /tmp/last_result.json
-
-	for url in "${urls[@]}"; do
+	while read url; do
 		echo $url
 		curl "$url" \
 			| grep pmcGalleryExports \
 			| sed "s/var pmcGalleryExports = \(.*\);/\1/" \
 			| jq '[.gallery[] | {"rank": .positionDisplay, "title": .title, "description": .description, "coverUrl": .image}]' \
-			> /tmp/curl_result.json
+			> $CURL_RESULT
 
-		jq -s '[ map(.[]) ]|flatten' /tmp/last_result.json /tmp/curl_result.json > /tmp/merge_result.json
+		jq -s '[ map(.[]) ]|flatten' $LAST_RESULT $CURL_RESULT > $MERGE_RESULT
 
-		cat /tmp/merge_result.json > /tmp/last_result.json
-	done
+		cat $MERGE_RESULT > $LAST_RESULT
+	done < $LINKS_FILE
 
-	mv /tmp/last_result.json ./greatest_500_albums.json
-	cleanup
+	mv $LAST_RESULT $OUTPUT_FILE
 }
 
 cleanup () {
-	rm /tmp/last_result.json
-	rm /tmp/curl_result.json
-	rm /tmp/merge_result.json
+	rm $LAST_RESULT
+	rm $CURL_RESULT
+	rm $MERGE_RESULT
 }
 
 main () {
+	get_links 2> /dev/null
 	execute 2> /dev/null
-	RESULT=$(cat greatest_500_albums.json | jq '.[] | .rank' | wc -l)
+	RESULT=$(cat $OUTPUT_FILE | jq '.[] | .rank' | sort | uniq -c | wc -l)
 	echo "$RESULT albums processed and added to ./greatest_500_albums.json"
 }
 
